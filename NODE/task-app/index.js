22db0dfe35230d9d5b2f313e753deb13f92b4aab
AyoubBenziza <ayoub.cbenziza@gmail.com>
2024-04-16 16:37:28 +0200

    add: Cors

--- NODE/task-app/index.js
+++ NODE/task-app/index.js
@@ -1,10 +1,13 @@
-const express = require('express');
+const express = require("express");
+const cors = require("cors");
 const app = express();
-const taskRouter = require("./router/taskRouter") 
+const taskRouter = require("./router/taskRouter");
+
+app.use(cors("*"));
 
 app.use(express.json());
 app.listen(3000, () => {
-  console.log('App running on port 3000');
+  console.log("App running on port 3000");
 });
 
-app.use("/task", taskRouter)
+app.use("/task", taskRouter);
